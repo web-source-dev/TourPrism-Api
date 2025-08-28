ffabfcbdea48871ba241102d4fd33d188f8e7ce3
RTNGLOBAL <risetonet@hotmail.com>
2025-08-29 03:12:00 +0800

    Adjusted

--- config/db.js
+++ config/db.js
@@ -2,19 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const mongoURI = process.env.MONGO_URI;
-    if (!mongoURI) {
-      throw new Error('MONGO_URI environment variable is not defined');
-    }
-    
-    await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      maxPoolSize: 10,
-      serverSelectionTimeoutMS: 5000,
-      socketTimeoutMS: 45000,
-    });
-    console.log("MongoDB Connected Successfully");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
   } catch (error) {
     console.error("MongoDB Connection Failed:", error);
     process.exit(1);
