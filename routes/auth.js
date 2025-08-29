e8ab2ada44ad029221c7d02b0c72fad38b58d95b
RTNGLOBAL <risetonet@hotmail.com>
2025-08-29 11:24:19 +0800

    Adjusted

--- routes/auth.js
+++ routes/auth.js
@@ -20,7 +20,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://tourprism-api.onrender.com/auth/google/callback",
+      callbackURL: "https://tourprism.onrender.com/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
